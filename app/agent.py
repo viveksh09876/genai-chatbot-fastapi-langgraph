4b3d0f67e84c99c11abf3f84abf4b07c8caca985
Vivek Sharma <viveksh09876@gmail.com>
2026-01-20 21:16:05 +0530

    test commit

--- app/agent.py
+++ app/agent.py
@@ -2,3 +2,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+GROQ_API_KEY=os.environ.get("GROQ_API_KEY")
+TAVILY_API_KEY=os.environ.get("TAVILY_API_KEY")
+OPENAI_API_KEY=os.environ.get("OPENAI_API_KEY")
+
+
