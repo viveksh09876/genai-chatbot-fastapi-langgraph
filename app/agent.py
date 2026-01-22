0c4ee54c0c5537bde35cd1bbca0538848dffa059
Vivek Sharma <viveksh09876@gmail.com>
2026-01-22 12:41:31 +0530

    agent configuration completed

--- app/agent.py
+++ app/agent.py
@@ -13,4 +13,43 @@ from langchain_community.tools.tavily_search import TavilySearchResults
 from langgraph.prebuilt import create_react_agent
 from langchain_core.messages.ai import AIMessage
 
+def get_response_from_ai_agent(
+    llm_id: str,
+    query: list[str],
+    allow_search: bool,
+    system_prompt: str,
+    provider: str
+) -> str: 
+    """
+    Returns response from AI agent based on:
+    - provider (Groq/OpenAI)
+    - model name
+    - optional Tavily search tool
+    """
+
+    if provider == "Groq":
+        llm  = ChatGroq(model=llm_id)
+    elif provider == "OpenAI":
+        llm = ChatOpenAI(model=llm_id)
+    else:
+        return "Invalid provider selected"
+
+    tools = [TavilySearchResults(max_results=2)] if allow_search else []
+
+    agent = create_react_agent(
+        model=llm,
+        tools=tools,
+        state_modifier=system_prompt
+    )
+
+    state = { "message": query }
+    response = agent.invoke(state)
+
+    messages = response.get("messages", [])
+
+    ai_messages = [
+        message.content for message in messages if isinstance(message, AIMessage)
+    ]
+
+    return ai_messages[-1] if ai_messages else "No response received"
 
